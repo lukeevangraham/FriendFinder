bb8988d81cea4a19761ee2a488ff586b05c2d1c5
Cefalu Wave <luke.evan.graham@gmail.com>
2019-05-20 22:50:10 -0700

    setting up Route Separation

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
-node_modules
-.DS_Store
+// Dependencies
+// =========================================
+
+var express = require("express");
+var path = require("path");
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes')(app);
+
+// Setup Express App
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
