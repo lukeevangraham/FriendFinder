d59441afb255c7c4087e2335a6be692f4b26a656
Cefalu Wave <luke.evan.graham@gmail.com>
2019-05-25 09:43:18 -0700

    setting up heroku deployment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require("path");
 
 // Setup Express App
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
